084d0ffc5e48fbd886fbbce7cc59a6a78cb21c62
snehalpathak <55033313+snehalpathak@users.noreply.github.com>
2019-11-25 10:42:24 -0800

    Create User.java
    Add user/customer details file

---
+++ VehicleManagementSystem/src/module4/team1/User.java
@@ -0,0 +1,50 @@
+package module4.team1;
+
+public class User {
+	private int userId;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String contactNo;
+	private String comment;
+	
+	
+	public int getCustomerId() {
+		return userId;
+	}
+	public void setCustomerId(int userId) {
+		this.userId = userId;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getContactNo() {
+		return contactNo;
+	}
+	public void setContactNo(String contactNo) {
+		this.contactNo = contactNo;
+	}
+	public String getComment() {
+		return comment;
+	}
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+	
+	
+}
